b27aef686a495578a931585a3fd34c79bc742330
Jeanluca Valencia <luca@Jeanlucas-Laptop.local>
2025-05-01 17:44:25 -0700

    Set up MongoDB Atlas connection using MONGODB_URI

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+dotenv.config();
 
 // Middleware
 app.use(express.json());
@@ -12,12 +16,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the main HTML file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Recipe routes
-const recipeRoutes = require('./routes/recipeRoutes');
+const recipeRoutes = require('./routes/recipes'); 
 app.use('/api/recipes', recipeRoutes);
+
+
+// Connect to MongoDB Atlas
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log(' Connected to MongoDB Atlas'))
+.catch(err => console.error(' MongoDB connection error:', err))
+
 
 // Start server
 app.listen(PORT, () => {
